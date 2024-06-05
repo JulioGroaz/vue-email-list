bdd57fd5988d8a5e73925d872bdd62d4c910f9f5
JulioGroaz <julio.groaz2020@gmail.com>
2024-06-05 15:41:54 +0200

    non avevo aggiunto lo script di axios

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 const { createApp } = Vue;
+
 createApp({
     data() {
-        return{
-            emails:[]
-        }
-
+        return {
+            emails: [] // Array per memorizzare le email
+        };
     },
-    methods:{
+    methods: {
         getData() {
+            // Creiamo un array di promesse per ottenere 10 email
             const emailPromises = [];
             for (let i = 0; i < 10; i++) {
                 emailPromises.push(axios.get('https://flynn.boolean.careers/exercises/api/random/mail'));
             }
+            
+            
             Promise.all(emailPromises)
-            .then((responses) => {
-                // Estrarre le email dalle risposte e aggiungerle all'array emails
-                responses.forEach(response => {
-                    this.emails.push(response.data.response);
-                });
-            })
+                .then((responses) => {
+                    // Estrarre le email dalle risposte e aggiungerle all'array emails
+                    this.emails = responses.map(response => response.data.response);
+                })
         }
-
     },
-    create(){
+    created() {
         this.getData();
-        }
+    }
 }).mount('#app');
